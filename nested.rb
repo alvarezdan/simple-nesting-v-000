e124df01429c8fde1903a6465d9e4ccb03f073b7
Daniel Alvarez <dnielalvrz@gmail.com>
2019-04-03 14:31:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,8 +102,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
-
+     #alans_new_info = "GUI"
+		 programmer_hash[:alan_kay][:known_for] = "GUI"
+		 puts programmer_hash
 
 end
 
@@ -123,6 +124,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:alan_kay][:known_for] = "GUI"
-puts programmer_hash
+
 end
